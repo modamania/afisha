3db6a1dfb0a28bec494dbfccdf763fef14b757d1
alekspetrov <mr.alex.petrov@gmail.com>
2015-04-14 09:51:15 +1000

    Basic setup with sass and project structure

--- Brocfile.js
+++ Brocfile.js
@@ -2,19 +2,30 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+	autoprefixer: {
+		browsers: ['last 2 version'],
+		cascade: false
+	}
+});
 
-// Use `app.import` to add additional libraries to the generated
-// output files.
-//
-// If you need to use different assets in different
-// environments, specify an object as the first parameter. That
-// object's keys should be the environment name and the values
-// should be the asset to use in that environment.
-//
-// If the library that you are including contains AMD or ES6
-// modules that you would like to import into your application
-// please specify an object with the list of modules as keys
-// along with the exports of each module as its value.
+var compileSass = require('broccoli-sass');
+var mergeTrees = require('broccoli-merge-trees');
 
-module.exports = app.toTree();
+// Sass src
+var sassSources = [
+	'app/styles'
+]
+
+// Compiling SASS
+var appCss = compileSass(sassSources, 'app.scss', 'assets/app.css', {
+	style: 'nested',
+	sourcemap: 'none'
+});
+
+// Merging trees
+var buildApp = mergeTrees([app.toTree(), appCss], {
+	overwrite: true
+});
+
+module.exports = buildApp;
--- config/environment.js
+++ config/environment.js
@@ -16,13 +16,23 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self'",
+      'font-src': "'self'",
+      'connect-src': "'self'",
+      'img-src': "'self'",
+      'style-src': "'self' 'unsafe-inline'",
+      'media-src': "'self'"
     }
   };
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
+    ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
