5ccc5d23f7112c42c0424e7f1c5d4f093ffac3e7
alekspetrov <mr.alex.petrov@gmail.com>
2015-06-01 04:06:41 +1000

    Index design

---
+++ app/models/event.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+var attr = DS.attr;
+export default DS.Model.extend({
+  	title: attr('string'),
+	date: attr('string'),
+	place: attr('string'),
+	description: attr('string'),
+	cover: attr('string'),
+	isHero: attr('boolean')
+});
--- server/mocks/events.js
+++ server/mocks/events.js
@@ -2,9 +2,57 @@ module.exports = function(app) {
   var express = require('express');
   var eventsRouter = express.Router();
 
+  var EVENTS = [
+    {
+      id: 1,
+      title: 'Back Street Boys',
+      cover: 'http://lorempixel.com/800/800/people',
+      date: '12 июня 2015',
+      place: 'СДК «Фетисов Арена»',
+      description: 'Fixie tote bag ethnic keytar. Neutra vinyl American Apparel kale chips tofu art party, cardigan raw denim quinoa. Cray paleo tattooed, Truffaut skateboard street art PBR jean shorts Shoreditch farm-to-table Austin lo-fi Odd Future occupy. Chia semiotics skateboard, Schlitz messenger bag master cleanse High Life occupy vegan polaroid tote bag leggings. Single-origin coffee mumblecore deep v salvia mlkshk. Organic photo booth cray tofu, vegan fixie bitters sriracha. Blog Austin Wes Anderson, deep v pour-over trust fund vinyl mlkshk +1.',
+      isHero: false
+    },
+    {
+      id: 2,
+      title: 'Уматурман',
+      cover: 'http://lorempixel.com/800/800/people',
+      date: '26 июня 2015',
+      place: 'СДК «Фетисов Арена»',
+      description: 'Fixie tote bag ethnic keytar. Neutra vinyl American Apparel kale chips tofu art party, cardigan raw denim quinoa. Cray paleo tattooed, Truffaut skateboard street art PBR jean shorts Shoreditch farm-to-table Austin lo-fi Odd Future occupy. Chia semiotics skateboard, Schlitz messenger bag master cleanse High Life occupy vegan polaroid tote bag leggings. Single-origin coffee mumblecore deep v salvia mlkshk. Organic photo booth cray tofu, vegan fixie bitters sriracha. Blog Austin Wes Anderson, deep v pour-over trust fund vinyl mlkshk +1.',
+      isHero: false
+    },
+    {
+      id: 3,
+      title: 'Группа Ленинград',
+      cover: 'http://lorempixel.com/800/800/people',
+      date: '16 июня 2015',
+      place: 'СДК «Фетисов Арена»',
+      description: 'Fixie tote bag ethnic keytar. Neutra vinyl American Apparel kale chips tofu art party, cardigan raw denim quinoa. Cray paleo tattooed, Truffaut skateboard street art PBR jean shorts Shoreditch farm-to-table Austin lo-fi Odd Future occupy. Chia semiotics skateboard, Schlitz messenger bag master cleanse High Life occupy vegan polaroid tote bag leggings. Single-origin coffee mumblecore deep v salvia mlkshk. Organic photo booth cray tofu, vegan fixie bitters sriracha. Blog Austin Wes Anderson, deep v pour-over trust fund vinyl mlkshk +1.',
+      isHero: false
+    },
+    {
+      id: 4,
+      title: 'Группа Ленинград',
+      cover: 'http://lorempixel.com/800/800/people',
+      date: '16 июня 2015',
+      place: 'СДК «Фетисов Арена»',
+      description: 'Fixie tote bag ethnic keytar. Neutra vinyl American Apparel kale chips tofu art party, cardigan raw denim quinoa. Cray paleo tattooed, Truffaut skateboard street art PBR jean shorts Shoreditch farm-to-table Austin lo-fi Odd Future occupy. Chia semiotics skateboard, Schlitz messenger bag master cleanse High Life occupy vegan polaroid tote bag leggings. Single-origin coffee mumblecore deep v salvia mlkshk. Organic photo booth cray tofu, vegan fixie bitters sriracha. Blog Austin Wes Anderson, deep v pour-over trust fund vinyl mlkshk +1.',
+      isHero: false
+    },
+    {
+      id: 5,
+      title: 'Группа Ленинград',
+      cover: 'http://lorempixel.com/800/800/people',
+      date: '16 июня 2015',
+      place: 'СДК «Фетисов Арена»',
+      description: 'Fixie tote bag ethnic keytar. Neutra vinyl American Apparel kale chips tofu art party, cardigan raw denim quinoa. Cray paleo tattooed, Truffaut skateboard street art PBR jean shorts Shoreditch farm-to-table Austin lo-fi Odd Future occupy. Chia semiotics skateboard, Schlitz messenger bag master cleanse High Life occupy vegan polaroid tote bag leggings. Single-origin coffee mumblecore deep v salvia mlkshk. Organic photo booth cray tofu, vegan fixie bitters sriracha. Blog Austin Wes Anderson, deep v pour-over trust fund vinyl mlkshk +1.',
+      isHero: false
+    }
+  ]
+  
   eventsRouter.get('/', function(req, res) {
     res.send({
-      'events': []
+      'events': EVENTS
     });
   });
 
@@ -13,10 +61,13 @@ module.exports = function(app) {
   });
 
   eventsRouter.get('/:id', function(req, res) {
+    // res.send({
+    //   'events': {
+    //     id: req.params.id
+    //   }
+    // });
     res.send({
-      'events': {
-        id: req.params.id
-      }
+      'events': EVENTS
     });
   });
 
